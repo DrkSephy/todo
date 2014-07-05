d89f2221afba47f9370c99797bfcc171df2d78e9
David Leonard <sephirothcloud1025@yahoo.com>
2014-07-05 17:35:13 -0400

    Adding configurations in server.js

---
+++ server.js
@@ -0,0 +1,14 @@
+var express     = require('express');
+var app         = express();
+var mongoose    = require('mongoose');
+
+mongoose.connect('mongodb://DrkSephy:seph1025@novus.modulusmongo.net:27017/yvurOr2y');
+
+app.configure(function(){
+    app.use(express.static(__dirname + '/public'));
+    app.use(express.logger('dev'));                 // log every request to the console
+    app.use(express.bodyParser());                  // pull information from html in POST
+});
+
+app.listen(8080);
+console.log("App listening on port 8080");
