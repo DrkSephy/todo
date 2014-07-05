ef9b478e45b297b2e94a5e100d44b387d03baf2f
David Leonard <sephirothcloud1025@yahoo.com>
2014-07-05 17:47:26 -0400

    Adding delete method for todos

--- server.js
+++ server.js
@@ -47,5 +47,22 @@ app.post('/api/todos', function(req, res){
         });
 });
 
+// delete a todo
+app.delete('/api/todos/:todo_id', function(req, res) {
+    Todo.remove({
+        _id : req.params.todo_id
+    }, function(err, todo) {
+        if (err)
+            res.send(err);
+
+        // get and return all the todos after you create another
+        Todo.find(function(err, todos) {
+            if (err)
+                res.send(err)
+            res.json(todos);
+        });
+    });
+});
+
 app.listen(8080);
 console.log("App listening on port 8080");
